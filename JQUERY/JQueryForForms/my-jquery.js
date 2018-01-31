d10bdcd0648031a60e80c13de6dd30146fa33c61
dimayasin <dyasin@gmail.com>
2018-01-31 15:33:58 -0600

    form and jquery

--- JQUERY/JQueryForForms/my-jquery.js
+++ JQUERY/JQueryForForms/my-jquery.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
     $('#fsubmit').on('click',function () {
         var first = $('input#fName').val();
         var last = $('input#lName').val();
+        var mail= $('input#email').val();
+        var cont=$('input#contact').val();
 
-        $('div#Results').append('<tr><td>'+first+' </td><td>'+last+'</td></tr>');
+        $('div#Results').append('<tr><td>'+first+' </td><td>'+last+' </td><td>'+mail+' </td><td>'+cont+'</td></tr>');
 
 
     });
